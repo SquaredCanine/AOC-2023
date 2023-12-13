a3de98540b65ba09e4510df6f53eac00177eca6a
Quinten Stekelenburg <quinten.stekelenburg@capgemini.com>
2023-12-13 14:41:30 +0100

    part2

--- src/main/kotlin/Day13.kt
+++ src/main/kotlin/Day13.kt
@@ -1,6 +1,23 @@
 package main.kotlin
 
+import kotlin.math.abs
+
 class Day13 {
+
+    fun getTestInput(): List<List<String>> {
+        val inputs = Util.getLinesFromFile("/day13/input.txt")
+        val indices = inputs.mapIndexed { index, list -> if (list.isEmpty()) index else null }.filter { it != null }
+        val patterns = MutableList(indices.size + 1) { mutableListOf<String>() }
+        var listIndex = 0
+        inputs.forEachIndexed { index, list ->
+            if (index in indices) {
+                listIndex++
+            } else {
+                patterns[listIndex].add(list)
+            }
+        }
+        return patterns
+    }
 
     fun List<String>.transpose(): List<String> {
         val cols = this[0].length
@@ -14,30 +31,46 @@ class Day13 {
         }
     }
 
-    fun String.isMirrored(splitLine: Int): Boolean {
+    val String.hashTags: Int get() = this.count { it == '#' }
+
+    fun String.isMirrored(splitLine: Int): Pair<Int, Boolean> {
         val diff = this.length - splitLine
         val start = (0).coerceIn(splitLine - diff, splitLine)
         val end = (splitLine + splitLine).coerceIn(splitLine, this.length)
         val first = this.substring(start, splitLine)
         val second = this.substring(splitLine, end).reversed()
         if (first.isEmpty() || second.isEmpty()) {
-            return false
+            return Pair(0, false)
         }
-        return first == second
+        return Pair(abs(first.hashTags - second.hashTags), first == second)
     }
 
-    fun List<String>.findMirror(multiplicationFactor: Long): Long {
+    fun List<String>.findMirror(multiplicationFactor: Long, isSmudged: Boolean): Long {
         for (i in 1 until this[0].length) {
-            if (this.all { it.isMirrored(i) }) {
+            var lenienceGiven = false
+            if (this.all {
+                    val result = it.isMirrored(i)
+                    val smudgeCount = result.first
+                    if (isSmudged) {
+                        if (smudgeCount == 1 && !lenienceGiven) {
+                            lenienceGiven = true
+                            true
+                        } else {
+                            result.second
+                        }
+                    } else {
+                        result.second
+                    }
+                } && (!isSmudged || lenienceGiven)) {
                 return i * multiplicationFactor
             }
         }
         return 0
     }
 
-    fun List<String>.mirrorMirrorOnTheWall(): Long {
-        val column = this.findMirror(1)
-        val row = this.transpose().findMirror(100)
+    fun List<String>.mirrorMirrorOnTheWall(isSmudged: Boolean = false): Long {
+        val column = this.findMirror(1, isSmudged)
+        val row = this.transpose().findMirror(100, isSmudged)
         return if (row > 0) {
             row
         } else if (column > 0) {
@@ -48,81 +81,21 @@ class Day13 {
     }
 
     fun doTheThing(): Long {
-        val inputs = Util.getLinesFromFile("/day13/input.txt")
-        val indices = inputs.mapIndexed { index, list -> if (list.isEmpty()) index else null }.filter { it != null }
-        println(indices)
-        val patterns = MutableList(indices.size + 1) { mutableListOf<String>() }
-        var listIndex = 0
-        inputs.forEachIndexed { index, list ->
-            if (index in indices) {
-                listIndex++
-            } else {
-                patterns[listIndex].add(list)
-            }
-        }
+        val patterns = getTestInput()
         return patterns.sumOf {
             it.mirrorMirrorOnTheWall()
         }
     }
 
-    fun Char.flip(): String {
-        return if (this == '#') {
-            "."
-        } else {
-            "#"
-        }
-    }
-
     fun doTheOtherThing(): Long {
-        val inputs = Util.getLinesFromFile("/day13/input.txt")
-        val indices = inputs.mapIndexed { index, list -> if (list.isEmpty()) index else null }.filter { it != null }
-        println(indices)
-        val patterns = MutableList(indices.size + 1) { mutableListOf<String>() }
-        var listIndex = 0
-        inputs.forEachIndexed { index, list ->
-            if (index in indices) {
-                listIndex++
-            } else {
-                patterns[listIndex].add(list)
-            }
+        val patterns = getTestInput()
+        return patterns.sumOf {
+            it.mirrorMirrorOnTheWall(true)
         }
-        return patterns.map {
-            val originalValue = it.mirrorMirrorOnTheWall()
-            val results = mutableListOf<Long>()
-            it.forEachIndexed { xIndex, str ->
-                str.forEachIndexed { yIndex, _ ->
-                    val new = it.toMutableList()
-                    val newValue = new[xIndex][yIndex].flip()
-                    new[xIndex] = new[xIndex].replaceRange(yIndex, yIndex + 1, newValue)
-                    val newLineValue = new.mirrorMirrorOnTheWall()
-                    if (newLineValue != 0L) {
-                        results.add(newLineValue)
-                    }
-                }
-            }
-            if (results.isEmpty()) {
-                println("Empty")
-                0L
-            } else {
-                if (results.all { it == results[0] }) {
-                    println("Expected")
-                    println(results)
-                    results[0]
-                } else {
-                    println("PANIIIICCCCCCCCCCCC!!!!!!")
-                    results
-                        .filter { it != originalValue }
-                        .also { println(it) }
-                        .first()
-                }
-            }
-        }
-            .also {}
-            .sum()
     }
 }
 
 fun main() {
     println(Day13().doTheThing())
-//    println(Day13().doTheOtherThing())
+    println(Day13().doTheOtherThing())
 }
