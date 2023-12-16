a7497b7f9b41190aff798345b6b033b76c3bd65d
Quinten Stekelenburg <quinten.stekelenburg@capgemini.com>
2023-12-16 13:53:10 +0100

    part 2 and total restructure lol

--- src/main/kotlin/Day16.kt
+++ src/main/kotlin/Day16.kt
@@ -4,9 +4,6 @@ class Day16 {
     data class Movement(val x: Int, val y: Int)
     data class Coordinate(val x: Int, val y: Int)
 
-    val globalTileMap = mutableMapOf<Pair<Coordinate, Movement>, Int>()
-    var temporaryGlobalMap = mutableMapOf<Pair<Coordinate, Movement>, Int>()
-
     fun getInput(): List<String> {
         return Util.getLinesFromFile("/day16/input.txt")
     }
@@ -21,15 +18,6 @@ class Day16 {
     fun Coordinate.isValid(layout: List<String>): Boolean {
         return this.x in layout[0].indices &&
                 this.y in layout.indices
-    }
-
-    fun lookAhead(nextCoordinate: Coordinate, movement: Movement, contraptionLayout: List<String>): Int {
-        val nextNextCoordinate = nextCoordinate.update(movement)
-        return if (nextNextCoordinate.isValid(contraptionLayout)) {
-            1
-        } else {
-            0
-        }
     }
 
     fun Char.alterMovement(movement: Movement): List<Movement> {
@@ -73,36 +61,29 @@ class Day16 {
         }
     }
 
-    fun takeStep(
+    fun figureItOut(
         currentLocation: Coordinate,
         movement: Movement,
-        contraptionLayout: List<String>,
-        visitedTileMap: MutableMap<Pair<Coordinate, Movement>, Int> = globalTileMap
+        contraptionLayout: List<String>
     ): Int {
-        visitedTileMap[Pair(currentLocation, movement)] =
-            visitedTileMap.getOrDefault(Pair(currentLocation, movement), 0) + 1
-        if (visitedTileMap[Pair(currentLocation, movement)]!! > 1) return 1
-        val nextCoordinate = currentLocation.update(movement)
-        //is Valid Location?
-        return if (nextCoordinate.isValid(contraptionLayout)) {
-            val movements = contraptionLayout[nextCoordinate.y][nextCoordinate.x].alterMovement(movement)
-            if (movements.size == 2) {
-                takeStep(nextCoordinate, movements.last(), contraptionLayout, visitedTileMap) +
-                        takeStep(nextCoordinate, movements.first(), contraptionLayout, visitedTileMap)
-            } else {
-                takeStep(nextCoordinate, movements.first(), contraptionLayout, visitedTileMap)
-            }
-        } else {
-            0
+        val tiles = Array(contraptionLayout.size) { contraptionLayout[0].map { false }.toMutableList() }
+        val seen = mutableSetOf<WomboCombo>()
+        var next = listOf(Pair(currentLocation, movement))
+        while (next.isNotEmpty()) {
+            next = next.mapNotNull { entry ->
+                val (coordinates, movement) = entry
+                if (!coordinates.isValid(contraptionLayout) || seen.contains(entry)) {
+                    null
+                } else {
+                    tiles[coordinates.y][coordinates.x] = true
+                    seen.add(entry)
+                    contraptionLayout[coordinates.y][coordinates.x].alterMovement(movement).map { nextMove ->
+                        Pair(coordinates.update(nextMove), nextMove)
+                    }
+                }
+            }.flatten()
         }
-    }
-
-    fun MutableMap<Pair<Coordinate, Movement>, Int>.printTheMap(input: List<String>) {
-        val map = input.map { ".".repeat(it.length) }.toMutableList()
-        this.entries.forEach { (coor, count) ->
-            map[coor.first.y] = map[coor.first.y].replaceRange(coor.first.x, coor.first.x + 1, "$count")
-        }
-        map.forEach { println(it) }
+        return tiles.sumOf { row -> row.count { it } }
     }
 
     val MutableMap<Pair<Coordinate, Movement>, Int>.score: Long
@@ -110,60 +91,37 @@ class Day16 {
             return this.keys.groupBy { it.first }.count().toLong()
         }
 
-    fun doTheThing(): Long {
+    fun doTheThing(): Int {
         val input = getInput()
-        try {
-            val result = input[0][0].alterMovement(Movement(1, 0)).sumOf {
-                takeStep(Coordinate(0, 0), it, input)
-            }
-            println(result)
-        } catch (error: StackOverflowError) {
-            globalTileMap.printTheMap(input)
-            println(globalTileMap.score)
-            System.exit(-10)
-        }
-        globalTileMap.printTheMap(input)
-        return globalTileMap.score
+        return figureItOut(Coordinate(0, 0), Movement(1, 0), input)
     }
 
-    fun doTheOtherThing(): Long {
+    fun doTheOtherThing(): Int {
         val input = getInput()
-        var max = -1L
-        try {
-            input.forEachIndexed { indexY, entry ->
-                listOf(0, entry.length - 1).forEach { startX ->
-                    val movX = if (startX == 0) 1 else -1
-                    input[indexY][startX].alterMovement(Movement(movX, 0)).forEach {
-                        temporaryGlobalMap = mutableMapOf()
-                        takeStep(Coordinate(indexY, startX), it, input, temporaryGlobalMap)
-                        if (temporaryGlobalMap.score > max) max = temporaryGlobalMap.score
-                    }
-                }
+        var max = -1
+        input.forEachIndexed { indexY, entry ->
+            listOf(0, entry.length - 1).forEach { startX ->
+                val movX = if (startX == 0) 1 else -1
+                val result = figureItOut(Coordinate(startX, indexY), Movement(movX, 0), input)
+                if (result > max) max = result
+
             }
-            input[0].forEachIndexed { indexX, str ->
-                listOf(0, input.size - 1).forEach { startY ->
-                    val movY = if (startY == 0) 1 else -1
-                    input[startY][indexX].alterMovement(Movement(0, movY)).forEach {
-                        temporaryGlobalMap = mutableMapOf()
-                        takeStep(Coordinate(startY, indexX), it, input, temporaryGlobalMap)
-                        if (temporaryGlobalMap.score > max) max = temporaryGlobalMap.score
-                    }
-                }
-            }
-        } catch (error: StackOverflowError) {
-            temporaryGlobalMap.printTheMap(input)
-            println(temporaryGlobalMap.score)
-            println(max)
-            System.exit(-10)
         }
-        temporaryGlobalMap.printTheMap(input)
+        input[0].forEachIndexed { indexX, str ->
+            listOf(0, input.size - 1).forEach { startY ->
+                val movY = if (startY == 0) 1 else -1
+                val result = figureItOut(Coordinate(indexX, startY), Movement(0, movY), input)
+                if (result > max) max = result
+            }
+        }
         return max
     }
 }
 
+typealias WomboCombo = Pair<Day16.Coordinate, Day16.Movement>
+
 fun main() {
     val asdf = Day16()
     println(asdf.doTheThing())
-    println("===========================================================================================")
     println(asdf.doTheOtherThing())
 }
